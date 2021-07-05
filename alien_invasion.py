0017517b29d4775abe9211e7e95b3ebe7492b6cc
Poorvi Vaish <img_2019047@iiitm.ac.in>
2021-07-05 18:49:41 +0530

    Refactored code

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
   pygame.init()
@@ -12,11 +13,7 @@ def run_game():
   bg_color = (230, 230, 230)
 
   while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        sys.exit()
-    screen.fill(ai_settings.bg_color)
-    ship.blitme()
-    pygame.display.flip()   
+    gf.check_events()
+    gf.update_screen(ai_settings, screen, ship) 
 run_game()
   
---
+++ game_functions.py
@@ -0,0 +1,13 @@
+import sys
+import pygame
+
+def check_events():
+  for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+
+def update_screen(ai_settings, screen, ship):
+  """Update images on the screen and flip to the new screen."""
+  screen.fill(ai_settings.bg_color)
+  ship.blitme()
+  pygame.display.flip()  
