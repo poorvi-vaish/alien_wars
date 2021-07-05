9e62064e2b9bcc094798cd9df53c1f4836bce835
Poorvi Vaish <img_2019047@iiitm.ac.in>
2021-07-05 17:00:46 +0530

    Added the initial screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,22 @@
 import sys
 import pygame
 from settings import Settings
-define run_game():
+from ship import Ship
+
+def run_game():
   pygame.init()
   ai_settings = Settings()
   screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
   pygame.display.set_caption("Alien Invasion")
+  ship = Ship(screen)
   bg_color = (230, 230, 230)
 
   while True:
     for event in pygame.event.get():
-      if event.type == pygme.QUIT:
+      if event.type == pygame.QUIT:
         sys.exit()
-    screen.fill(ai_settings.bg_clor)
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
     pygame.display.flip()   
 run_game()
   
