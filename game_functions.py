c3bd3b52fada2ae74463d5f18ffb5597747f84c7
Poorvi Vaish <img_2019047@iiitm.ac.in>
2021-07-06 19:55:52 +0530

    Added keyboard events

--- game_functions.py
+++ game_functions.py
@@ -1,13 +1,37 @@
 import sys
 import pygame
 
-def check_events():
-  for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        sys.exit()
+
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Respond to keyreleases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+def check_events(ship):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            print ('keypressed')
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            print (event.key)
+            check_keyup_events(event, ship)
+
 
 def update_screen(ai_settings, screen, ship):
-  """Update images on the screen and flip to the new screen."""
-  screen.fill(ai_settings.bg_color)
-  ship.blitme()
-  pygame.display.flip()  
+    """Update images on the screen and flip to the new screen."""
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    pygame.display.flip()
